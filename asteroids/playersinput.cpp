88a154675537a9a375bb1c33c5ae4b4ef9924f1e
alex.dwgpl <alex.dwgpl@t10.pw>
2017-03-01 10:21:10 +0200

    developing playerInput class

--- asteroids/playersinput.cpp
+++ asteroids/playersinput.cpp
@@ -1,7 +1,83 @@
 #include "playersinput.h"
+#include "aircraft.h"
+
+struct AircraftMover {
+
+    AircraftMover(float vx, float vy)
+        : velocity(vx, vy)
+    {
+    }
+
+    void operator() (Aircraft& aircraft, sf::Time) const
+    {
+        aircraft.setVelocity(velocity);
+    }
+
+    sf::Vector2f velocity;
+};
 
 PlayersInput::PlayersInput()
+    : mCurrentMissionStatus(MissionRunning)
+{
+    mKeyBinding[sf::Keyboard::A] = MoveLeft;
+    mKeyBinding[sf::Keyboard::D] = MoveRight;
+    mKeyBinding[sf::Keyboard::W] = MoveUp;
+    mKeyBinding[sf::Keyboard::S] = MoveDown;
+
+    initializeActions();
+
+    for (auto& pair : mActionBinding) {
+        pair.second.category = Category::PlayerAircraft;
+    }
+}
+
+void PlayersInput::handleEvent(const sf::Event& event,
+                            CommandQueue& commands)
 {
 
 }
 
+void PlayersInput::handleRealtimeInput(CommandQueue& command)
+{
+
+}
+
+void PlayersInput::setMissionStatus(MissionStatus status)
+{
+    mCurrentMissionStatus = status;
+}
+
+PlayersInput::MissionStatus PlayersInput::getMissionStatus() const
+{
+    return mCurrentMissionStatus;
+}
+
+void PlayersInput::initializeActions()
+{
+    mActionBinding[MoveLeft].action  = derivedAction<Aircraft>(AircraftMover(-1,  0));
+    mActionBinding[MoveRight].action = derivedAction<Aircraft>(AircraftMover(+1,  0));
+    mActionBinding[MoveUp].action    = derivedAction<Aircraft>(AircraftMover( 0, -1));
+    mActionBinding[MoveDown].action  = derivedAction<Aircraft>(AircraftMover( 0, +1));
+    mActionBinding[Fire].action      = derivedAction<Aircraft>(
+                                    [] (Aircraft& a, sf::Time)
+                                    {
+                                        //a.fire();
+                                    });
+
+}
+
+bool PlayersInput::isRealtimeAction(Action action)
+{
+    switch (action)
+    {
+        case MoveLeft:
+        case MoveRight:
+        case MoveDown:
+        case MoveUp:
+        case Fire:
+            return true;
+
+        default:
+            return false;
+    }
+}
--- asteroids/playersinput.h
+++ asteroids/playersinput.h
@@ -1,13 +1,49 @@
 #ifndef PLAYERSINPUT_H
 #define PLAYERSINPUT_H
 
+#include "command.h"
+
+#include <SFML/Window/Event.hpp>
+
+#include <map>
 
 class PlayersInput
 {
 public:
+    enum Action {
+
+        MoveLeft,
+        MoveRight,
+        MoveUp,
+        MoveDown,
+        Fire,
+        ActionCount
+    };
+
+    enum MissionStatus {
+
+        MissionRunning,
+        MissionSuccess,
+        MissionFailue
+    };
+
+public:
     PlayersInput();
 
+    void            handleEvent(const sf::Event& event,
+                                CommandQueue& commands);
+    void            handleRealtimeInput(CommandQueue& command);
 
+    void            setMissionStatus(MissionStatus status);
+    MissionStatus   getMissionStatus() const;
+
+    void            initializeActions();
+    bool            isRealtimeAction(Action action);
+
+private:
+    std::map<sf::Keyboard::Key, Action> mKeyBinding;
+    std::map<Action, Command>           mActionBinding;
+    MissionStatus                       mCurrentMissionStatus;
 };
 
 #endif // PLAYERSINPUT_H
